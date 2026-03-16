67a0c552a8d65257b31d59a58e86019e1879d7db
Halloran, Donald (Corp) <Donald.Halloran@coateshire.com.au>
2026-03-16 11:45:04 +1100

    references

--- GoFold/GoFold.cs
+++ GoFold/GoFold.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.ComponentModel.Design;
-using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio;
 using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.Editor;
 using Microsoft.VisualStudio.TextManager.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
